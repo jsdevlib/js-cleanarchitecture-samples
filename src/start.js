a130c421ef9521a7aa6d04bf426482ace769ce9a
Alberto Arroyo Raygada <beyondnet.peru@gmail.com>
2023-08-19 02:16:17 -0500

    adding mongoDb

--- src/start.js
+++ src/start.js
@@ -17,9 +17,9 @@ if (process.env.NODE_ENV === 'development') {
 
 connectDB();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
-server.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
+server.listen(PORT, console.log(`Server running in port ${PORT}`.yellow.bold));
 
 // Handle unhandled promise rejections
 process.on('unhandledRejection', (err, promise) => {
